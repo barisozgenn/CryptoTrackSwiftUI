5d1e13bb0ae5dc308a70466204a445a109d3b85c
Barış Özgen <barisozgen@hotmail.com>
2022-09-28 12:57:35 +0300

    refactor : tabview

--- CryptoTrackSwiftUI/CryptoTrackSwiftUI/Core/Root/Views/RootView.swift
+++ CryptoTrackSwiftUI/CryptoTrackSwiftUI/Core/Root/Views/RootView.swift
@@ -11,35 +11,38 @@ struct RootView: View {
     
     @EnvironmentObject var launchScreenManager : LaunchScreenViewModel
     
-    @StateObject private var viewModel = RootViewModel()
+    //@StateObject private var marketViewModel = MarketViewModel()
     
     var body: some View {
-        ZStack{
-            NavigationView{
-                TabView{
-                    MarketView()
-                        .tabItem {
-                            Image(systemName: "chart.xyaxis.line")
-                            Text("Market")
-                        }
-                    
-                    PortfolioView()
-                        .tabItem {
-                            Image(systemName: "chart.pie")
-                            Text("Portfolio")
-                        }
-                }
-                .onAppear{
-                    DispatchQueue
-                        .main
-                        .asyncAfter(deadline: .now() + 1.74 ) {
-                            launchScreenManager.dismiss()
-                        }
-                }
+        NavigationView{
+            TabView{
+                MarketView()
+                    .tabItem {
+                        Image(systemName: "chart.xyaxis.line")
+                        Text("Market")
+                    }
+                
+                PortfolioView()
+                    .tabItem {
+                        Image(systemName: "chart.pie")
+                        Text("Portfolio")
+                    }
+                    .toolbarBackground(.red, for: .tabBar)
+            }
+            .onAppear{
+                
+                UITabBar.appearance().backgroundColor = .systemGroupedBackground
+                
+                DispatchQueue
+                    .main
+                    .asyncAfter(deadline: .now() + 1.74 ) {
+                        launchScreenManager.dismiss()
+                    }
             }
         }
-      
-       
+        
+        
+        
     }
 }
 
